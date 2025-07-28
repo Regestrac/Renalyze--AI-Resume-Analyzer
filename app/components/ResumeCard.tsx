88a277152e1860bf154ac46884d5d06f0d9c7547
Sojo C Johny <sojocjohny37@gmail.com>
2025-07-28 17:59:56 +0530

    Added recently analyzed resume list as cards.

---
+++ app/components/ResumeCard.tsx
@@ -0,0 +1,30 @@
+import { Link } from "react-router";
+
+const ResumeCard = ({ resume }: { resume: Resume }) => {
+  const { id, companyName, jobTitle, imagePath, feedback } = resume;
+
+  return (
+    <Link to={`/resume/${id}`} className="resume-card animate-in fade-in duration-1000">
+      <div className="resume-card-header">
+        <div className="flex flex-col gap-2">
+          <h2 className="!text-black font-bold break-words">{companyName}</h2>
+          <h3 className="text-lg break-words text-gray-500">{jobTitle}</h3>
+        </div>
+        <div className="flex-shrink-0">
+          {feedback.overallScore}/100
+        </div>
+      </div>
+      <div className="gradient-border animate-in fade-in duration-1000">
+        <div className="w-full h-full">
+          <img
+            src={imagePath}
+            alt="Resume"
+            className="w-full h-[350px] max-sm:h-[200px] object-cover object-top"
+          />
+        </div>
+      </div>
+    </Link>
+  )
+}
+
+export default ResumeCard;
